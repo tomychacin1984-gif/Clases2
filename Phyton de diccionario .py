c0c308f2754c708922283aab02749262458dd049
tomychacin1984-gif <tomychacin1984@gmail.com>
2025-07-25 20:24:13 -0400

    Add files via upload

---
+++ Phyton de diccionario .py
@@ -0,0 +1,55 @@
+# crea un diccionario 
+ #mi_diccionario= ()
+nombre = "suarez"
+edad= 30
+carrera = "empresario"
+
+print(carrera)
+
+#contar frecuencias de letras
+"phyton"
+palabra= "banana"
+
+#atualizer valores
+"python"
+mi_diccionario=()
+nombre= "manzana"
+nombre= "banana"
+
+
+# diccionario contador
+mi_diccionario = ()
+"phyton"
+{0, 1, 2, 3, 4}
+
+#actualizar valores 
+mi_diccionario = ()
+"phyton"
+nombre= "manzana"
+valor= 0.80
+print("valor")
+nombre= "banana"         
+valor=1,40
+print("valor")
+
+#iterar valores en phyton
+mi_diccionario= ()
+"phyton"
+pais= {"Italia"} 
+capital= ("Roma")
+print= ("pais")
+pais= ("españa")
+capital= ("madrid")
+print= ("pais")
+pais=" el {Italia} es {Roma}"
+pais= "el {españa} es {madrid}"
+
+
+  
+ 
+
+ 
+ 
+ 
+
+
